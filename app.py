262abdcb69db43812b9a8a0b6581b0dc7eb2de8b
WeiTsungCheng <sanctuary85c@gmail.com>
2022-05-04 01:35:09 +0800

    [feat] add memo

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask_jwt_extended import JWTManager
 
 from db import db
 
-from resources.user import UserRegister, User, UserLogin, UserLogout, TokenRefresh #, AdminUserRegister
+from resources.user import UserRegister, User, UserLogin, UserLogout, TokenRefresh
 from resources.addressbook import AddressBook
 from resources.contactItem import ContactItem, ContactItemList
 
@@ -18,6 +18,9 @@ app = Flask(__name__)
 # 切記要先 load 才取得到 .env 的資料
 load_dotenv()
 
+# 如果從 Heroku 上拿不到環境變數則取本地端的 Server 位置
+# 因為 .env 並不會上傳到 Github 上
+
 if os.environ.get('DATABASE_URL'):
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL').replace("postgres://", "postgresql://", 1)
 else:
@@ -25,6 +28,10 @@ else:
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
+
+# 如果從 Heroku 上拿不到環境變數則取本地端的 SECRET_KEY 位置
+# 因為 .env 並不會上傳到 Github 上
+# 另外 secret_key 一定要設, 因為做為 flask-jwt-extended 加密的鹽
 
 if os.environ.get('SECRET_KEY'):
     app.secret_key = os.environ.get('SECRET_KEY')
@@ -47,15 +54,13 @@ def check_if_token_in_blacklist(jwt_header, jwt_payload):
 
 api.add_resource(UserRegister, '/register')
 api.add_resource(User, '/user/info')
-
-# api.add_resource(User, '/user/<uuid:user_id>')
 api.add_resource(UserLogin, '/login')
 api.add_resource(TokenRefresh, '/refresh')
 api.add_resource(UserLogout, "/logout")
 api.add_resource(AddressBook, "/user/addressbook/<string:bookname>")
 api.add_resource(ContactItem, "/user/addressbook/<string:bookname>/contactitem/<string:itemname>")
 api.add_resource(ContactItemList, "/user/addressbook/<string:bookname>/contactitems")
-# api.add_resource(AdminUserRegister, "/admin/register")
+
 
 if __name__ == '__main__':
     db.init_app(app)
